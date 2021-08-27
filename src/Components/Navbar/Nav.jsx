6d7c8ac24438b3bdb3b157541e96040a28981a43
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-08-27 12:45:42 +0900

    change icon

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
             alignItems: 'center'
         }} 
         className="nav-container">
-            <h1 style={{color: 'black', fontFamily: 'Nova Square', margin: '0 0 0 5vw', padding: '0 20px'}}>NOVA TERRA</h1>
+            <h1 style={{color: 'black', fontFamily: 'Nova Square', margin: '0 0 0 5vw', padding: '0 20px'}}>NOVA</h1>
         </div>
     )
 }
