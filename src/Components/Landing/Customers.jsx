3b8037f3c8c5ac88ecfa1c9e430807066e0dafe6
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-09-02 13:45:21 +0900

    Nova -> Mine

--- src/Components/Landing/Customers.jsx
+++ src/Components/Landing/Customers.jsx
@@ -60,7 +60,7 @@ const Customers = () => {
             </h1>
             <div style={customerStepContainerStyles(width)}>
                 <CustomerCard image="https://miro.medium.com/max/1400/1*w1ZgjY_U0NyoEVlh9vFfXA.png" paragraph="1. Leverage the Terra Ecosystem to enjoy lossless subscriptions for all your contents."/>
-                <CustomerCard image="https://paganresearch.io/images/terraformlabs.png" paragraph="2. Stake NOVA token for a chance to win NFT content drops from your favorite creators."/>
+                <CustomerCard image="https://paganresearch.io/images/terraformlabs.png" paragraph="2. Stake MINE token for a chance to win NFT content drops from your favorite creators."/>
                 <CustomerCard image="https://s2.coinmarketcap.com/static/img/coins/200x200/4172.png" paragraph="3. Bid LUNA token to buy exclusive videos and photos on special occasions." />
             </div>
         </div>
